df191600e4d285dc46b6c92872656d2f5a8a1ffc
Stefanie Cruz <stefanie.cruz.vazquez@gmail.com>
2023-10-05 10:40:31 -0700

    Upload Local Changes

---
+++ intro.py
@@ -0,0 +1,103 @@
+# # print("hey it's me ")
+
+# from cmath import pi
+# from operator import truediv
+
+
+# name = "mike"
+# num = 47
+# pi = 3.1415
+
+# if num > 30:
+#     print("that numer is kinda big")
+
+
+# isSunny = True
+# isRanny = False
+
+# num = [1, 2, 3, 4, 5, 6]  # list
+# num.append(108)
+# print(len(num))
+# print(num[0])
+# num.pop()
+
+# sundae = {  # dictionary
+#     "flavor": "mint chocolate chip",
+#     "numScoops": 2,
+#     "toppings": ["brownie", "not fudge", "chocolate shavings", "whipped cream"]
+# }
+# print(sundae["flavor"])
+
+# # funtion
+
+
+# def sum(a, b):
+#     print("finding the sum of the num")
+#     return a + b
+
+
+# print(sum(4, 2))
+
+# if isRanny or pi > 3:
+#     print(f"hi {name},glad to see you!")
+# elif name == "mike":
+#     print("hi {name},glad to see you!")
+
+# # num2= 23
+# # num3 = "30"
+# #  print(num + int(num3))
+# #  newStr = "my number is :" +
+
+# # for (var i=0; i<arr.lemght;i++)
+# #  i=0
+# #  while(i<10):
+# #     print(i)
+# #     i +=1
+
+
+# num4 = [4, 5, 7, 8, 0]
+# for number in num4:
+#     print(number)
+
+# for x in range(10):
+#     print (x)
+
+# for f in range(1000, 0, -5):
+#     print (f)
+
+# myStrp = "hey its me"
+# for letter  in myStrp:
+#     print(letter)
+
+# name = "Zen"
+# num = 23
+ #print("My name is", num)
+print("My name is")
+
+# name2 = "Zen"
+# print("My name is " + num)
+
+# Practice Challenge: Correct the errors!
+#first_name = "Alana"
+#last_name = "Da Silva"
+#age = 36
+#profession = "Software Developer"
+#years_experience = 5
+
+#greeting = "Hello my name is ", first_name, last_name
+
+#print(greeting) 
+ # Desired output: Hello my name is Alana Da Silva
+
+#print(f"I am {age} years old") 
+# # Desired output: I am 36 years old
+
+#print("I work as a {}.".format(profession))
+# # Desired output: I work as a Software Developer.
+
+#exp_string = "I have worked in the field for {} years."
+#print(exp_string.format(years_experience))
+# # Desired output: I have worked in the field for 5 years.
+
+#print(f"I started in the field when I was { age - years_experience}  years old.")
+# # Desired output: I started in the field when I was 31 years old.
